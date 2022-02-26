ef7a61d586bd2136916233d29372031193358b33
murtrag <lukasz.czernik@gmail.com>
2022-02-26 17:48:13 +0100

    Added battery engine

--- dashboard/utils/system.py
+++ dashboard/utils/system.py
@@ -10,14 +10,64 @@ _cmd = subprocess.check_output
 
 class Battery:
     # print('test',  subprocess.check_output(['ls', '-l']) )
+    dummy_bat = '''
+  native-path:          BAT1
+  vendor:               SDI
+  model:                PA5185U-1BRS
+  power supply:         yes
+  updated:              Sat 26 Feb 2022 05:00:39 PM CET (33 seconds ago)
+  has history:          yes
+  has statistics:       yes
+  battery
+    present:             yes
+    rechargeable:        yes
+    state:               fully-charged
+    warning-level:       none
+    energy:              31.536 Wh
+    energy-empty:        0 Wh
+    energy-full:         31.536 Wh
+    energy-full-design:  31.68 Wh
+    energy-rate:         7.1568 W
+    voltage:             14.407 V
+    percentage:          100%
+    capacity:            99.5455%
+    technology:          lithium-ion
+    icon-name:          'battery-full-charged-symbolic'
+  History (rate):
+    1645891239	7.157	fully-charged
+    '''
+    def __fetch_bat_data(self):
+        def __split_elems(line):
+            try:
+                key, val = line.split(": ") 
+                return {key.strip(): val.strip()}
+            except ValueError:
+                # This is for lines which doasn't have pair key:val like title val and a History
+                return {}
+
+        cmd_out = Battery.dummy_bat.strip()
+        cmd_split = cmd_out.split('\n')
+        # cmd_to_dict = {**__split_elems(line) for line in cmd_split}
+        cmd_to_dict = dict() 
+        for line in cmd_split:
+            cmd_to_dict.update(__split_elems(line))
+        return cmd_to_dict
+
     def getCapacity(self):
-        return str(randint(0,100))
+        # bat_data = self.__fetch_bat_data()
+        return self.__fetch_bat_data()['percentage'][:-1]
+        # return str(randint(0,100))
 
     def getIconName(self):
         pass
 
     def getTimePrediction(self):
-        return f"{randint(0,3)}hour {randint(0,59)}min"
+        # pass
+        try:
+            return self.__fetch_bat_data()['time to empty']
+        except KeyError:
+            return "-----"
+        # return f"{randint(0,3)}hour {randint(0,59)}min"
     
 
 class SysInfo:
@@ -150,9 +200,14 @@ if '__main__' == __name__:
     # print( t.level )
     # t.level = 100
 
-    from pprint import pprint
-    t = SysInfo()
+    # from pprint import pprint
+    # t = SysInfo()
     # pprint(t.coresTemp())
-    pprint( t.getDisksInfo() )
+    # pprint( t.getDisksInfo() )
+
+    t = Battery()
+    print(
+        t.getCapacity()
+    )
 
 
