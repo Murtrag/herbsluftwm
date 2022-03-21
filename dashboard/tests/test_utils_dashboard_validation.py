811c1e9188dde636b9a93ffbfc2ede35c525359f
Murtrag-laptop <lukasz.czernik@gmail.com>
2022-03-21 20:24:45 +0100

    begin to work on chain test

--- dashboard/tests/test_utils_dashboard_validation.py
+++ dashboard/tests/test_utils_dashboard_validation.py
@@ -4,3 +4,9 @@ from utils import dashboard_validation as df
 def test_is_afk():
     assert callable(df.is_afk), "Is afk doasn't seem like a function"
     assert df.is_afk() in (True, False)
+
+
+
+#def test_stat_chain(query):
+def test_stat_chain():
+   stat_chaing.handle(query) 
